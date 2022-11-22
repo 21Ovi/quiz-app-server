1784a5b3a87b794eaab287e0af4afdee0ac6b821
= <=>
2022-11-23 00:11:39 +0530

    advanced setup setted

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
 
 const app = express();
+
+// app middlewares
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+config();
+
+// application port
+const port = process.env.PORT || 8080;
 
 // routes
 app.get("/", (req, res) => {
@@ -11,6 +23,6 @@ app.get("/", (req, res) => {
   }
 });
 
-app.listen(8080, () => {
-  console.log(`server connected to http://localhost:8080`);
+app.listen(port, () => {
+  console.log(`server connected to http://localhost:${port}`);
 });
