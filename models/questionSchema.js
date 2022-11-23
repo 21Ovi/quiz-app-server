2e2771ecaa1d5f9ddc59e1919a945f7b06f506c4
= <=>
2022-11-24 00:11:26 +0530

    models added

---
+++ models/questionSchema.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+// Question Model
+const questionModel = new Schema({
+  questions: { type: Array, default: [] },
+  answers: { type: Array, default: [] },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Questions", questionModel);
